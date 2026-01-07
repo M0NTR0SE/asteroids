3fa3f121abe2f81108f61fbb5c80ddb9900bfb20
M0NTR0SE <charles.bougard013@gmail.com>
2026-01-06 20:51:32 -0500

    added clock, dt calculation

--- main.py
+++ main.py
@@ -7,20 +7,35 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # initialize game
     pygame.init()
 
+    # set screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # start clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        # log game state
         log_state()
 
+        # monitor for user closing window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # fill screen black
         screen.fill("black")
 
+        # refresh screen
         pygame.display.flip()
+
+        # tick clock 60 FPS
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        # print(f"{dt}")
 
 if __name__ == "__main__":
     main()
