ab92867c4642c590c47aa2c02d9c61398ba08aab
M0NTR0SE <charles.bougard013@gmail.com>
2026-01-13 00:05:10 -0500

    new circleshape collision detection function

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        sum_radius = self.radius + other.radius
+
+        if self.position.distance_to(other) <= sum_radius:
+            return True
+        return False
